8fd6bcb3bce6ab2f56f4ab04cc9016242cbb324c
Omkar Pathak <omkarpathak27@gmail.com>
2026-05-07 18:37:44 +0530

    feat: add author field to blog model, update SEO/schema tags, and implement read time calculation

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.utils.text import slugify
+from django.conf import settings
 
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True, blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
     summary = models.TextField(help_text="A short summary of the blog post")
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-    keywords = models.CharField(max_length=255, blank=True, help_text="Comma-separated keywords for SEO")
+    keywords = models.CharField(max_length=1000, blank=True, help_text="Comma-separated keywords for SEO")
 
     class Meta:
         ordering = ['-published_date']
@@ -21,3 +23,8 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
+
+    @property
+    def read_time(self):
+        word_count = len(self.content.split())
+        return max(1, word_count // 200)
